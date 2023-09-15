44667ba7feac392f8d7d50244a5e0d17fe73e049
pouya817 <m.pouya.ch@gmail.com>
2023-09-15 12:24:32 +0330

    add put delete and patch basic api view

--- api_conf/views.py
+++ api_conf/views.py
@@ -26,3 +26,15 @@ class helloapi(APIView):
         else :
             return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
         
+    def put (self , request , pk=None):
+        """handles updating an object."""
+        return Response({"method":"put"})
+    
+    def patch (self , request , pk=None):
+        """patch request, only updates fields provided in the request."""
+        return Response({"method":"patch"})
+    
+    def delete (self , request , pk=None):
+        """deletes something."""
+        return Response({"method":"delete"})
+    
